e78667a9554a8b9e4924b434d8fd572f989ef792
Roy de Kleijn <roy.de.kleijn@powerhouse.nl>
2017-10-19 20:52:59 +0200

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AssistantListAppComponent} from "./assistant-list.component";
-import {MatButtonModule, MatCheckboxModule, MatInputModule} from "@angular/material";
+import {
+  MatButtonModule, MatCardModule, MatCheckboxModule, MatIconModule, MatInputModule, MatListModule, MatSidenavModule,
+  MatToolbarModule
+} from "@angular/material";
 import {FormsModule} from "@angular/forms";
 import {AssistantService} from "./shared/services/assistant/assistant.service";
 import {HttpClientModule} from "@angular/common/http";
@@ -14,9 +17,19 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     HttpClientModule,
-    MatButtonModule, MatCheckboxModule, MatInputModule, FormsModule
+    MatButtonModule, MatCheckboxModule, MatInputModule, FormsModule, MatCardModule, MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+
   ],
-  exports: [MatButtonModule, MatCheckboxModule, MatInputModule],
+  exports: [MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+],
   providers: [AssistantService],
   bootstrap: [AssistantListAppComponent]
 })
--- src/app/shared/services/assistant/assistant.service.ts
+++ src/app/shared/services/assistant/assistant.service.ts
@@ -1,6 +1,4 @@
 import {Injectable} from '@angular/core';
-import {Response,} from '@angular/http';
-
 import {Assistants} from '../../model/assistants.model';
 import {Observable} from 'rxjs/Rx';
 import {HttpClient, HttpParams} from "@angular/common/http";
@@ -17,13 +15,7 @@ export class AssistantService {
     return this.http.get<any>(this.USER_URL, {
       params: new HttpParams().set('results', number)
     })
-      .map(this.extractData)
       .catch(this.handleError);
-  }
-
-  private extractData(res: Response) {
-    let body = res.json();
-    return body || {};
   }
 
   private handleError(error:any) {
